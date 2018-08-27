798ebab535ae6048afbedc62abef87f389a676e4
Spencer Dent <mr.spencerdent@gmail.com>
2018-08-26 20:10:07 -0400

    the texture shader error is gone, but the screen is still black

--- progress/src/GLSLShaders/TextureFS.glsl
+++ progress/src/GLSLShaders/TextureFS.glsl
@@ -4,7 +4,8 @@ precision mediump float;  // sets the precision for floating point computation
 // must be set outside texture
 uniform sampler2D uSampler;
 
-uniform vec4 uPixelColor; // to transform the fragment color
+// to transform the fragment color
+uniform vec4 uPixelColor;
 
 // interpolated
 varying vec2 vTexCoord;
@@ -15,7 +16,7 @@ void main(void) {
 
   // tint the textured area. leave transparent area as defined by texture
   vec3 r = vec3(c) * (1.0-uPixelColor.a) + vec3(uPixelColor) * uPixelColor.a;
-  vec4 result = (r, c.a);
+  vec4 result = vec4(r, c.a);
 
   gl_FragColor = result;
 }
