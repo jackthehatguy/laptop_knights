80720dacf346bfb4c3d4a83fb150637417fd31e0
Spencer Dent <mr.spencerdent@gmail.com>
2019-01-30 21:42:14 -0500

    $

--- workshop/utils.js
+++
@@ -1,7 +0,0 @@
-const max = (a, b) => a > b ? a : b > a ? b : undefined;
-
-const sum = (a,b) => a+b;
-const summation = a => a.reduce(sum)
-
-const isEven = x => !isNaN(x) && !(x%2);
-const isOdd = x => !isNaN(x) && !isEven(x);
