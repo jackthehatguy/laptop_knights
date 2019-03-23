b619aaa33624b2eaa6ea68b2e6d0d46f79fceecc
Spencer Dent <mr.spencerdent@gmail.com>
2019-03-22 22:10:20 -0400

    +ShadowSim

--- src/MyGame/MyGame_Lights.js
+++ src/MyGame/MyGame_Lights.js
@@ -15,6 +15,7 @@ MyGame.prototype._createALight = function (type, pos, dir, color, n, f, inner, o
   light.setOuter(outer);
   light.setIntensity(intensity);
   light.setDropOff(dropOff);
+  light.setLightCastShadowTo(true);
 
   return light;
 };
@@ -25,53 +26,55 @@ MyGame.prototype._initializeLights = function () {
 
   l = this._createALight(
     Light.eLightType.ePointLight, // type
-    [15, 50, 5],                  // position
-    [0, 0, -1],                   // direction
-    [1.0, 0.0, 0.0, 1],           // color
+    [20, 25, 10],                 // position
+    [0, 0, -1],                   // direction: not used
+    [1, 0.8, 0.5, 1],             // color
     8, 20,                        // near & far
-    0.1, 0.2,                     // inner & outer cones
+    0.1, 0.2,                     // inner & outer cones: not used
+    3,                            // intensity
+    1.0                           // drop off: not used
+  );
+  this.mGlobalLightSet.addToSet(l);
+
+  l = this._createALight(
+    Light.eLightType.eDirectionalLight, // type
+    [0, 0, 0],                          // position: not used
+    [0.4, 0.4, -1],                     // direction
+    [0.3, 0.3, 0.3, 1],                 // color
+    0, 0,                               // near & far: not used
+    0, 0,                               // inner & outer cones: not used
+    2,                                  // intensity
+    0                                   // drop-off: not used
+  );
+  l.setLightTo(false);  // turns off light
+  this.mGlobalLightSet.addToSet(l);
+
+  l = this._createALight(
+    Light.eLightType.eSpotLight,  // type
+    [65, 25, 12],                 // position
+    [0, 0, -1],                   // direction
+    [0.5, 0.5, 0.5, 1],           // color
+    10, 25,                       // near & far
+    1.5, 2.2,                     // inner & outer cones
     5,                            // intensity
-    1.0                           // drop off
-  );
-  this.mGlobalLightSet.addToSet(l);
-
-  l = this._createALight(
-    Light.eLightType.eDirectionalLight,
-    [15, 50, 4],
-    [-0.2, -0.2, -1],
-    [0.5, 0.5, 0.5, 1],
-    500, 500,
-    0.1, 0.2,
-    2,
-    1.0
+    1.2                           // drop-off
   );
   this.mGlobalLightSet.addToSet(l);
 
   l = this._createALight(
     Light.eLightType.eSpotLight,
-    [80, 18, 10],
-    [-0.07, 0, -1],
-    [0.0, 0.0, 1.0, 1],
-    100, 100,
-    1.65, 1.7,
-    5,
-    1.2         
-  );
-  this.mGlobalLightSet.addToSet(l);
-
-  l = this._createALight(
-    Light.eLightType.eSpotLight,
-    [64, 43, 10],
-    [0.0, 0.03, -1],
-    [0.0, 1.0, 0.0, 1],
-    100, 100,
-    1.9, 2.0,
+    [60, 50, 12],
+    [0.02, -0.02, -1],
+    [0.8, 0.8, 0.2, 1],
+    20, 40,
+    1.2, 1.3,
     2,
-    1
+    1.5
   );
   this.mGlobalLightSet.addToSet(l);
 };
 
-MyGame.prototype._applyAllLights = function (renderable) {
-  for (let i = 0; i < this.mGlobalLightSet.mSet.length; i++) renderable.addLight(this.mGlobalLightSet.getLightAt(i));
+// added by Spencer
+MyGame.prototype._applyAllLights = function (...renderables) {
+  this.mGlobalLightSet.mSet.map(lgt => renderables.map(renderable => renderable.addLight(lgt)));
 };
